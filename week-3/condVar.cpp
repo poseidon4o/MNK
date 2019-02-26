39e70a96a458f8c87983189c5e84ce31262ffd0d
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2019-02-26 16:00:41 +0200

    Fix cond var example to actually work

--- week-3/condVar.cpp
+++ week-3/condVar.cpp
@@ -1,16 +1,16 @@
 #include <thread>
 #include <condition_variable>
 #include <mutex>
+#include <random>
 
 #include <Windows.h>
 
 const int iterations = 3;
-const int thCount = 5;
+const int thCount = 10;
 
 int progress[thCount] = {0,};
 std::mutex mtx;
 std::condition_variable event;
-
 
 bool canProgress(int threadIndex) {
 	const int ourProgress = progress[threadIndex];
@@ -27,20 +27,33 @@ bool canProgress(int threadIndex) {
 }
 
 void print(int threadIndex) {
+	std::mt19937 mt(threadIndex);
+
 	for (int c = 0; c < iterations; c++) {
-		Sleep(rand() % 500);
+
+		// simulate work taking some time
+		float f = 0;
+		{
+			const int count = mt() % 100000000 + 1000000;
+			for (int r = 0; r < count; r++) {
+				f += log10(f + r) * pow(f + r, 3.14) * sqrt(f + r);
+			}
+		}
+
 		std::unique_lock<std::mutex> lock(mtx);
-		if (canProgress(threadIndex)) {
-			printf("Thread [%d] -> %d\n", threadIndex, c);
-		} else {
+		if (!canProgress(threadIndex)) {
 			event.wait(lock, [threadIndex]() {
 				return canProgress(threadIndex);
 			});
 		}
 
+		// report and log progress
+		printf("Thread [%f] [%d] -> %d\n", f, threadIndex, progress[threadIndex]);
+		progress[threadIndex]++;
+
 		lock.unlock();
+		// notify_one or notify_all
 		event.notify_one();
-		
 	}
 }
 
