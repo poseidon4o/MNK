0e1e7eb818f217defd585d32ef3a5326190f20dd
Lyubomir Koev <poseidon4o.random@gmail.com>
2019-02-27 22:28:24 +0200

    Add genetic algo declarations

---
+++ homework-3/flappy.h
@@ -0,0 +1,33 @@
+#include <vector>
+
+struct Point2d {
+    float x, y;
+};
+
+const float FPS = 60.f;
+
+const float HORIZONTAL_VELOCITY = 0.6 / FPS;
+const float VERTICAL_ACCELERATION = 9.8 / FPS;
+const float JUMP_ACCELERATION = VERTICAL_ACCELERATION * 2.f;
+
+struct LevelDescription {
+    /// Total length of the level
+    float length;
+    /// Total height of the level
+    float height;
+    struct Pilon {
+        /// Center is between [0, 0] and [width, height]
+        /// Refers to the point in the middle of the gap between top and bottom part of the pilon
+        Point2d center;
+        /// Width of the pilon
+        float width;
+        /// The hight of the gap between top and bottom part of the pilon
+        float gapHeight;
+    };
+
+    /// Each pilon in the level
+    std::vector<Pilon> pilons;
+};
+
+/// Expected number of decisions is (level.width / (HORIZONTAL_VELOCITY * FPS))
+std::vector<bool> getAgentDecisions(const LevelDescription &level);
